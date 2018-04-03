f39800945a519a96a324f8dfc2599b060cb90466
Sean-Akh <sean.akhlaghi@gmail.com>
2018-04-03 13:12:37 -0700

    updated CSS and app.js functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,6 +47,8 @@ $(document).ready(function(){
 
     //create query request, submit, process response and facilitate the display of information
     $(document).on("click", ".mvButton", function(){
+        $('#FunnyMovies').empty();
+
         var movie =$(this).text();
         var key = "api_key=dDKdue88ZmaMAeboaltAX6uGX8tgryjS";
         var queryURL = "https://api.giphy.com/v1/gifs/search?" + key + "&q=" + movie.split(' ').join('-') + "&limit=10&lang=en"; 
@@ -64,33 +66,44 @@ console.log('The Query is: ' + queryURL);
             //process the response
 
         var results = response.data;
-
-console.log(results);            
-
-            //place the response in #funny-animation-box
-            for(var j = 0; j < results.length; j++) {
-                //create the div
-                var faDiv = $('<div>');
-                faDiv.addClass('item');
-                faDiv.attr('id', results[j].id);
-                //add the image
-                var faImage = $("<img>");
-                faImage.attr("src", results[j].images.original_still.url);
-                faImage.addClass('pic-class');
-                faDiv.append(faImage);
-                
-                //add rating of the movie
-                faDiv.append($("<p>").text("rating: " + results[j].rating));
-//faDiv.append($("<p>").text('<p id="description">' + results[j].slug));
-
-                $("#FunnyMovies").prepend(faDiv);
+console.log(results);    
+            if(results.length ===0) {
+                $('#FunnyMovies').text('Sorry your there is nothing about your request');
             }
-
+            else {
+                //place the response in #funny-animation-box
+                for(var j = 0; j < results.length; j++) {
+                    //create the div
+                    var faDiv = $('<div>');
+                    faDiv.addClass('item');
+                    faDiv.attr('id', results[j].id);
+                    //add the image
+                    var faImage = $('<img>');
+                    faImage.attr("src", results[j].images.original_still.url);
+                    faImage.attr("image-still", results[j].images.original_still.url);
+                    faImage.attr("image-animate", results[j].images.original.url);
+                    faImage.attr("image-state", "still")
+                    faImage.addClass('pic-class');
+                    faDiv.append(faImage);
+                    
+                    //add rating of the movie
+                    faDiv.append($("<p>").text("rating: " + results[j].rating));
+                    $("#FunnyMovies").prepend(faDiv);
+                }
+            }
         });
         //Create the animation and still transition
-       // if (faImage.state === "still") {
-
-        //}
+        $(document).on("click", ".pic-class", function(){
+            var state = $(this).attr('image-state')
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("image-animate"));
+                $(this).attr("image-state", "animate");
+            }
+            else{
+                $(this).attr("src", $(this).attr("image-still"));
+                $(this).attr("image-state", "still");
+            }
+        });
     });
 
     
